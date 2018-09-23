f718576abbc29e5b60bfda6cdf187a87c41922ac
hoang anh <hoanganhctm7@gmail.com>
2018-09-24 06:41:05 +0700

    state and props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>test</h1>
-        <Person name='hoang anh'>My hobbie is playing game</Person>
-        <Person name='ngan ha'></Person>
-        <Person name='dau dau'></Person>
-      </div>
-    )
-  }
+    state = {
+        persons: [
+            {name: 'hoang anh', age: 29},
+            {name: 'ngan ha', age: 29},
+            {name: 'dau dau', age: 2},
+        ]
+    };
+
+    switchNameHandler = (newName) => {
+        this.setState({
+            persons: [
+                {name: newName, age: 29},
+                {name: 'ngan ha', age: 29},
+                {name: 'dau dau', age: 2},
+            ]
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>test</h1>
+                <button onClick={this.switchNameHandler.bind(this, 'new hoang anh')}>Switch name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}>My hobbie is playing game</Person>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}>
+
+                </Person>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}>
+                </Person>
+            </div>
+        )
+    }
 }
 
 export default App;
