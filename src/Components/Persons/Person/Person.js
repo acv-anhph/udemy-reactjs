d7d2824ef7e9162777d873311a61235e45f7f385
hoang anh <hoanganhctm7@gmail.com>
2018-10-02 22:14:53 +0700

    done 89

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -2,7 +2,21 @@ import React, {Component} from 'react';
 import PersonClasses from './Person.css';
 
 class Person extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    console.log('[person.js] in constructor', props);
+  }
+
+  componentWillMount() {
+    console.log('[person.js] in componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('[person.js] in componentDidMount');
+  }
+
+  render() {
+    console.log('[person.js] in render');
     return (
       <div className={PersonClasses.Person}>
         <p onClick={this.props.click}>My name is {this.props.name} and I'm {this.props.age} years old</p>
--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -2,7 +2,44 @@ import React, {Component} from 'react';
 import Person from "./Person/Person";
 
 class Persons extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[persons.js] in constructor', props);
+  }
+
+  componentWillMount() {
+    console.log('[persons.js] in componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('[persons.js] in componentDidMount');
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('[Update Persons.js] inside componentWillReceiveProps', nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Update Persons.js] inside shouldComponentUpdate', nextProps, this.props.persons, nextState);
+    return nextProps.persons !== this.props.persons ||
+           nextProps.clicked !== this.props.clicked ||
+           nextProps.changed !== this.props.changed;
+    // return true;
+  }
+
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[Update Persons.js] inside componentWillUpdate', nextProps, nextState);
+  }
+
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('[Update Persons.js] inside componentDidUpdate', prevProps, prevState);
+  }
+
+
   render() {
+    console.log('[persons.js] in render');
     return this.props.persons.map((person, index) => {
       return <Person
         key={person.id}
